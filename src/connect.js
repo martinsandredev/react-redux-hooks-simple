5369b3886e6e99bf53380fd1c84e40d329f91e72
André Martins <35877866+martinsandredev@users.noreply.github.com>
2019-04-10 00:09:14 -0300

    Eliminate unnecessary content

--- src/connect.js
+++ src/connect.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useRedux } from "./store";
-// import PropTypes from "prop-types";
 
 export default (mapState = state => state, mapActions = {}) => Component => {
   const ReduxConnect = props => {
